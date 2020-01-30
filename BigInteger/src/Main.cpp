873d032ecb41b3f6dafc514f410cf322de2391d1
Vojtěch Michal <michavo3@fel.cvut.cz>
2020-01-30 11:13:07 +0100

    Initial commit.

---
+++ BigInteger/src/Main.cpp
@@ -0,0 +1,18 @@
+//#include "BigInteger.h"	
+//#include <iostream>
+//#include <vector>
+//#include <array>
+
+
+#if 0
+
+int main(int argc, char**argv) {
+	std::vector<int> vec_;
+}
+	#endif
+#include <vector>
+int main() {
+	std::vector<int> vec;
+	char v = vec.begin();
+
+ }
